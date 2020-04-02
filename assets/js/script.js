0b0b10874e345e42a8f3bff25ac7285116168067
saniketprdxn <anikets@prdxn.com>
2020-04-02 05:26:07 +0000

    comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,83 @@
 /* Author: Aniket*/
-// Defining Variables
+// Getting Required DOM elements
 const coin = ['heads', 'tails'];
 const playerSelection = document.querySelector('.player-selection');
 const computerSelection = document.querySelector('.computer-selection');
 const playerScore = document.querySelector('.player-score');
 const computerScore = document.querySelector('.computer-score');
 const coinImg = document.querySelector('.coin img');
-const coinFig = document.querySelector('.coin figure');
-const btns = Array.from(document.querySelectorAll('.toss-call li'));
+const coinFigure = document.querySelector('.coin figure');
+const buttons = Array.from(document.querySelectorAll('.toss-call li'));
 
-function init(btns,playerScore,computerScore,playerSelection,computerSelection) {
-btns.forEach((btn) => btn.addEventListener('click',flip));
-playerScore.innerText = "0";
-computerScore.innerText = "0";
-playerSelection.innerText = "nothing";
-computerSelection.innerText = "nothing";
+// Initial function which sets the stage for game to initial state
+function init(buttons, playerScore, computerScore, playerSelection, computerSelection) {
+	buttons.forEach((button) => button.addEventListener('click', flip));
+	playerScore.innerText = "0";
+	computerScore.innerText = "0";
 }
 
-init(btns,playerScore,computerScore,playerSelection,computerSelection);
+init(buttons, playerScore, computerScore, playerSelection, computerSelection);
 
-function flip(){
+// Flip function to calculate random behaviour of a coin toss 
+function flip() {
 	playerSelection.innerText = this.innerText;
-	if(playerSelection.innerText === coin[0]) {
-			computerSelection.innerText = coin[1];
+	if (playerSelection.innerText === coin[0]) {
+		computerSelection.innerText = coin[1];
 	} else {
-			computerSelection.innerText = coin[0];
+		computerSelection.innerText = coin[0];
 	}
-	let coinFlip = coin[Math.floor(Math.random()*coin.length)];
+	/* Math.random provides values from 0 to 1 if we didn't provide any values
+		to paranthesis, we multiplied it with the length of an array we created above,
+		to get multiples of random value as length is 2 then we floor it to make it 
+		either zero or one to get either heads or tails.*/
+	let coinFlip = coin[Math.floor(Math.random() * coin.length)];
 	coinImgFlip(coinFlip);
 }
 
+// Function to flip the Coin Image by changing it source
 function coinImgFlip(flipAnswer) {
-	coinFig.classList.add('flipping');
-	if(flipAnswer === coin[0]) {
-		coinImg.src = "./assets/Images/"+coin[0]+".jpg";
+	coinFigure.classList.add('flipping');
+	if (flipAnswer === coin[0]) {
+		coinImg.src = "./assets/Images/" + coin[0] + ".jpg";
 	} else {
-		coinImg.src = "./assets/Images/"+coin[1]+".jpg";
+		coinImg.src = "./assets/Images/" + coin[1] + ".jpg";
 	}
 	setTimeout(() => {
-		coinFig.classList.remove('flipping');
-		updatescore(flipAnswer);
-	},1300)
+		coinFigure.classList.remove('flipping');
+		updateScore(flipAnswer);
+	}, 1300)
 }
 
-function updatescore(coinFlipAns) {
-	if(coinFlipAns === playerSelection.innerText) {
+// function to update scores till it reaches to max 5(winning Condition)
+function updateScore(coinFlipAns) {
+	if (coinFlipAns === playerSelection.innerText) {
 		playerScore.innerText++;
-	} else if(coinFlipAns === computerSelection.innerText){
+	} else if (coinFlipAns === computerSelection.innerText) {
 		computerScore.innerText++;
 	}
-	if(playerScore.innerText == 5 ) {
+	if (playerScore.innerText == 5) {
 		win(playerScore);
-	}	else if(computerScore.innerText == 5) {
+	} else if (computerScore.innerText == 5) {
 		win(computerScore);
 	}
 }
 
+// Win function if anyone wins it's name gets Alerted
 function win(winner) {
 	let theWinner;
-	if(winner.classList.contains('player-score')) {
+	if (winner.classList.contains('player-score')) {
 		theWinner = "player";
 	} else {
 		theWinner = "computer";
 	}
 	setTimeout(() => {
-		alert("winner of the game is: "+theWinner);
+		alert("winner of the game is: " + theWinner);
 		playAgain();
-	},100)
+	}, 100)
 }
 
-function playAgain(){
+// play Again function if Player wants to play again 
+function playAgain() {
 	alert("Play again?");
 	window['location'].reload();
 }
